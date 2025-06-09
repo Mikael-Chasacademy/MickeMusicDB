22af6ae71568daa37c16c761671ffaa163d1e712
Mikael-Chasacademy <mikael.linderoth@chasacademy.se>
2025-06-09 17:54:25 +0200

    update
    Tog bort onödig kod

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,17 +1,13 @@
-import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { store } from "../store/store";
 import "../styles/globals.css";
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
   );
 }
 
